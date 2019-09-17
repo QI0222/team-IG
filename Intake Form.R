75aa18a1abbdaae85d5ff857712adabb88e4d3c7
Xinci Chen <chenxinci@dhcp-wifi-8021x-155-41-102-54.bu.edu>
2019-09-17 16:34:05 -0400

    commit on

---
+++ Intake Form.R
@@ -0,0 +1,25 @@
+MSSP Consulting: Limited Duration Project
+Description and Intended Scope
+Client:  Kelly
+MSSP Team: IG
+Intake Date: 09/17/2019
+
+Purpose of the consulting: The purpose of this consulting arrangement is 1) to train both MSSP and PhD students on the process of statistical consultation and 2) to provide a service to improve the quantitative aspects of research/projects on Boston University’s campus.  Student consultants will be monitored by MSSP faculty in order to help the consultants provide their best possible service.  Nevertheless, we ask that you understand that students are not (yet!) professional consultants and we thank you for your patience and cooperation in this regard.  
+
+Scope of the project: The project described below has been approved for what we call “limited duration” consultation.  As the name suggests, such projects are intended to be fairly focused, with a total duration of no more than 10 student hours worth of work. Consultation will involve MSSP student consultants and PhD leaders, supervised by MSSP program faculty.  An initial intake meeting should have been held in the MSSP Conference Room at 2 Cummington Mall and additional meetings, as needed, will be arranged at the mutual convenience of the client and the student consultants.       
+
+Project description: 
+  Based on the information provided during the intake meeting, our understanding of the client project is as follows.
+●	The client wants to know 
+
+
+Initial milestone:
+  To address the client needs, our proposal is to provide the following by MM/DD/YYYY.
+●	Details of the first mile stone.
+●	Code?
+  ●	Analysis?
+  ●	Report?
+  
+  Attribution: When the scope of the work is of `limited duration’ but nevertheless is used in any sort of academic product (e.g., presentation, project, abstract, or publication), we would ask that you please include an appropriate acknowledgment. For example, clients may include “We acknowledge the help of [NAME] of the MSSP statistical consulting service under the guidance of Professor [NAME].” as a footnote accompanying grant support and related .
+Modification of Intended Scope of Work: At any time during this process if both parties mutually agree that the above proposal is suboptimal in addressing the client needs we leave open the possibility of holding additional discussions to alter the above proposal.  Consultations anticipated to require substantially more than roughly 10 hours of work may be recommended for collaborative consulting.  Evaluation of such recommendations typically will include a discussion of co-authorship on resulting end-products.
+
