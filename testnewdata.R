e8723af3c965189bdfa476e5d46c52714eae784a
Hailey <hailey@dhcp-wifi-8021x-168-122-202-97.bu.edu>
2019-11-21 13:35:03 -0500

    newdata test

---
+++ testnewdata.R
@@ -0,0 +1,20 @@
+#New data
+pacman::p_load(readxl,dplyr,tidyverse,ggplot2,plotly,ggpubr,MASS,fitdistrplus,mediation,geepack,conf,car,knitr,gclus,scatterplot3d, ppcor)
+newdata <- read.csv("new data.csv")
+summary(newdata)
+data1 <- filter(newdata,Year=="2017")
+#data transformation: generate a new variable called GV to measure the level of racial diversity
+data2 <- data1 %>% 
+  mutate(Racial_diversity=1-White_E^2-Black_E^2-Hispanic_E^2) %>%
+  mutate(norm_Racial_diversity = Racial_diversity*(3/2))
+summary(data2)
+#Check the Cr plots for test model
+fittest <- lm(Voe~Union+Urbanity+Unemployment+GDP+Totalpop,data = data2)
+summary(fittest)
+plot(fittest)
+crPlots(fittest)
+
+#transformation
+fit1 <- lm(log(Voe)~Union+Urbanity+log(Unemployment)+log(GDP)+log(Totalpop),data=data2)
+summary(fit1)
+plot(fit1)
