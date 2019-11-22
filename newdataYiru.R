fcd4f581ff60ffb4bca1a50e913966ccca8bc4a3
feiyiru <54414770+feiyiru@users.noreply.github.com>
2019-11-22 16:09:24 -0500

    add norm_RD

--- newdataYiru.R
+++ newdataYiru.R
@@ -12,28 +12,33 @@ newdata <- newdata[,1:19]
 data = na.omit(newdata)
 
 data = data%>% 
-  mutate( Other = Totalpop-Black_E-White_E-Hispanic_E)
-data = data %>%
-  mutate(Racial_diversity = 1-White_E^2-Black_E^2-Hispanic_E^2-Other^2)
+  mutate(Black = Black_E/Totalpop)
+data = data%>%
+mutate(White = White_E/Totalpop)
+data = data%>%
+mutate(Hispanic = Hispanic_E/Totalpop)
 
 data = data %>%
-  mutate(norm_RD = Racial_diversity *(4/3))
+  mutate(Racial_diversity = 1-White^2-Black^2-Hispanic^2)
+
+data = data %>%
+  mutate(norm_RD = Racial_diversity *(3/2))
 
 
 
 
-fit1 = lm(Voe~GDP+factor(Year)+Urbanity+Unemployment+Totalpop+Union,data = data)
+fit1 = lm(Voe~GDP+factor(Year)+Urbanity+Unemployment+Totalpop+Union+ giniE,data = data)
 summary(fit1)
 plot(fit1)
 crPlots(fit1)
 
 
-fit2 = lm(log(Voe)~log(GDP)+factor(Year)+Urbanity+log(Unemployment)+log(Totalpop)+Union,data = data)
+fit2 = lm(log(Voe)~log(GDP)+factor(Year)+Urbanity+log(Unemployment)+log(Totalpop)+Union+log(giniE),data = data)
 summary(fit2)
 plot(fit2)
 crPlots(fit2)
 
-fit3 = lm(log(Voe)~log(GDP)+Urbanity+Unemployment+log(Totalpop)+Union + scale(norm_RD),data = data)
+fit3 = lm(log(Voe)~log(GDP)+Urbanity+Unemployment+log(Totalpop)+Union + scale(norm_RD)+log(giniE),data = data)
 summary(fit3)
 plot(fit3)
 crPlots(fit3)
